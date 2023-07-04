b0f79afdd4d41a7bd9bc56d3aea781bb2a0eb6ee
Gyuri <haha0888@naver.com>
2023-07-04 17:34:48 +0900

    0704_17:32

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function login_page() {
 function registeration_page() {
   window.location.href = "registeration.html";
 }
+
+const nonClick = document.querySelectorAll(".non-click");
+function handleClick(event) {
+  nonClick.forEach((e) => {
+    e.classList.remove("click");
+  });
+  event.target.classList.add("click");
+}
+nonClick.forEach((e) => {
+  e.addEventListener("click", handleClick);
+});
+
+const createDir = function () {
+  const newDir = document.createElement("button");
+  directory.appendChild(newDir);
+};
